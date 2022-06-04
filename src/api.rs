06dc2947232246f9e2cc6e9c878c3e03b50fe3dc
nixalpha <n1xalphaa@gmail.com>
2022-06-03 19:56:25 -0700

    Add one more skeleton endpoint Also includes some slight code refactoring

--- src/api.rs
+++ src/api.rs
@@ -2,8 +2,16 @@ use super::handlers;
 use warp::Filter;
 
 pub fn api() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    warp::path!("api" / "misc" / "ref")
+    let contact_endpoint = warp::path!("api" / "misc" / "contact")
         .and(warp::post())
-        .and(handlers::mail_request())
-        .and_then(handlers::send_mail)
+        .and(handlers::contact_request())
+        .and_then(handlers::send_contact_email);
+
+    let refer_endpoint = warp::path!("api" / "misc" / "ref")
+        .and(warp::post())
+        .and(handlers::refer_request())
+        .and_then(handlers::send_refer_email);
+
+    contact_endpoint
+        .or(refer_endpoint)
 }
--- src/handlers.rs
+++ src/handlers.rs
@@ -2,18 +2,33 @@ use serde::{Serialize, Deserialize};
 use warp::Filter;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct MailRequest {
+pub struct ContactRequest {
     pub subject: String,
     pub body: String,
 }
 
-pub async fn send_mail(req: MailRequest) -> Result<impl warp::Reply, warp::Rejection> {
+pub fn contact_request() -> impl Filter<Extract = (ContactRequest,), Error = warp::Rejection> + Clone {
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
+
+pub async fn send_contact_email(req: ContactRequest) -> Result<impl warp::Reply, warp::Rejection> {
     println!("Subject: {}", req.subject);
     println!("Body: {}", req.body);
 
     Ok(warp::reply::reply())
 }
 
-pub fn mail_request() -> impl Filter<Extract = (MailRequest,), Error = warp::Rejection> + Clone {
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct ReferRequest {
+    pub ref_code: String,
+}
+
+pub fn refer_request() -> impl Filter<Extract = (ReferRequest,), Error = warp::Rejection> + Clone {
     warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
+
+pub async fn send_refer_email(req: ReferRequest) -> Result<impl warp::Reply, warp::Rejection> {
+    println!("Refer code: {}", req.ref_code);
+
+    Ok(warp::reply::reply())
 }
