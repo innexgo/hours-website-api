bfe3e3211fbdcfd7b98a2dd39d859721bd55ecc1
nixalpha <n1xalphaa@gmail.com>
2022-06-04 10:15:49 -0700

    Add clap + the beginnings of mail-service

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
+use clap::Parser;
+use mail_service_api::client::MailService;
+
 mod api;
 mod handlers;
 
+#[derive(Parser, Debug, Clone)]
+#[clap(about, version, author)]
+struct Opts {
+    #[clap(short, long)]
+    port: u16,
+    #[clap(short, long)]
+    mail_service_url: String,
+}
+
 #[tokio::main]
 async fn main() {
+    let Opts {
+        port,
+        mail_service_url,
+    } = Opts::parse();
+
+    let mail_service = MailService::new(&mail_service_url).await;
+
     let api = api::api();
 
     warp::serve(api)
-        .run(([127, 0, 0, 1], 3030))
+        .run(([127, 0, 0, 1], port))
         .await;
 }
